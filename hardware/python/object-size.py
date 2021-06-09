5b4b637335555f1880e6ab080361ee8947e36729
edwinmwiti <emwiti658@gmail.com>
2021-06-09 10:19:07 +0300

    filtered the remaining contours

--- hardware/python/object-size.py
+++ hardware/python/object-size.py
@@ -55,4 +55,12 @@ for cnt in cnts:
     box = np.array(box, dtype="int")
     box = perspective.order_points(box)
     (tl, tr, br, bl) = box
-    cv2.
+    cv2.drawContours(image, [box.astype("int")], -1, (0,0,255), 2)
+    mid_pt_horizontal = (tl[0] + int(abs(tr[0] - tl[0])/2), tl[1] + int(abs(tr[1] - tl[1])/2))
+    mid_pt_verticle = (tr[0] + int(abs(tr[0] - br[0])/2), tr[1] + int(abs(tr[1] - br[1])/2))
+    wid = euclidean(tl, tr)/pixel_per_cm
+    ht = euclidean(tr, br)/pixel_per_cm
+    cv2.putText(image, "{:.1f}cm".format(wid), (int(mid_pt_horizontal[0] - 15), int(mid_pt_horizontal[1] - 10)), 
+		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 2)
+    cv2.putText(image, "{:.1f}cm".format(ht), (int(mid_pt_verticle[0] + 10), int(mid_pt_verticle[1])), 
+		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 2)
